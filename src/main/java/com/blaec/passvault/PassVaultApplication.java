dc2b4ee05986829e33ede86c6963ae1147b7045e
blaec <odeskonst@yandex.ru>
2022-01-08 11:35:03 +0200

    first commit

---
+++ src/main/java/com/blaec/passvault/PassVaultApplication.java
@@ -0,0 +1,13 @@
+package com.blaec.passvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blaec/passvault/PassVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaec.passvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
