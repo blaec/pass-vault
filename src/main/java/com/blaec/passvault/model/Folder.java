59a11273a391f08dea060fe064e4f282ea085379
blaec <odeskonst@yandex.ru>
2022-01-10 22:32:05 +0200

    #1 create model objects

---
+++ src/main/java/com/blaec/passvault/model/Folder.java
@@ -0,0 +1,21 @@
+package com.blaec.passvault.model;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+@Slf4j
+@Entity
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "folders")
+public class Folder {
+
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+
+    @NonNull private String folder;
+}
