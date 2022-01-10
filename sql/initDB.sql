e6d0968d5790e167f30b57c9e8ffef01304e7bc7
blaec <odeskonst@yandex.ru>
2022-01-10 22:22:07 +0200

    #1 create tables

---
+++ sql/initDB.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS folders;
+DROP TABLE IF EXISTS passwords;
+
+CREATE TABLE folders
+(
+    id              INT auto_increment,
+    folder          VARCHAR(100) 			    NULL,
+    PRIMARY KEY     (id)
+);
+ALTER TABLE folders AUTO_INCREMENT = 100000;
+
+
+CREATE TABLE passwords
+(
+    id              INT auto_increment,
+    folder_id       INT 			            NULL,
+    title           VARCHAR(100) 			    NOT NULL,
+    user            VARCHAR(100) 			    NOT NULL,
+    password        VARCHAR(100) 			    NOT NULL,
+    website         VARCHAR(100) 			    NOT NULL,
+    note            VARCHAR(300) 			    NULL,
+    creation_date   DATE     					NOT NULL,
+    FOREIGN KEY (folder_id) REFERENCES folders (id),
+    PRIMARY KEY     (id)
+);
+
+ALTER TABLE passwords AUTO_INCREMENT = 100000;
