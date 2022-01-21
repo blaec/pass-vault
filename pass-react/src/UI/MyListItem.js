7d459009d7a58ae51cc094b3cb4f9f66ef85bd4b
blaec <odeskonst@yandex.ru>
2022-01-21 22:35:04 +0200

    #7 add routes

--- pass-react/src/UI/MyListItem.js
+++ pass-react/src/UI/MyListItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 import {ListItem, ListItemIcon, ListItemText} from "@material-ui/core";
 
 const myListItem = (props) => {
-    const {caption, icon} = props;
+    const {caption, icon, link} = props;
 
     return (
         <ListItem
             button
+            component={NavLink} to={link}
         >
             <ListItemIcon>{icon}</ListItemIcon>
             <ListItemText primary={caption}/>
