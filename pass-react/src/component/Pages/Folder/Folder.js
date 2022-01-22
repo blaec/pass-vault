0171bf2f01645973c185ce01a3728ce21ee09a3a
blaec <odeskonst@yandex.ru>
2022-01-22 18:06:45 +0200

    #16 add list of folders

--- pass-react/src/component/Pages/Folder/Folder.js
+++ pass-react/src/component/Pages/Folder/Folder.js
@@ -1,47 +1,61 @@
 import React from 'react';
 
-import {Button, TextField} from "@mui/material";
-import CreateNewFolderTwoToneIcon from '@mui/icons-material/CreateNewFolderTwoTone';
+import {styled} from '@mui/material/styles';
+import {Avatar, Grid, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from '@mui/icons-material/Delete';
+import FolderIcon from '@mui/icons-material/Folder';
 
+function generate(element) {
+    return [0, 1, 2].map((value) =>
+        React.cloneElement(element, {
+            key: value,
+        }),
+    );
+}
+
+const Demo = styled('div')(({ theme }) => ({
+    backgroundColor: theme.palette.background.paper,
+}));
 
 const Folder = () => {
+    const dense = false;
+    const secondary = false;
+
     return (
-        <React.Fragment>
-            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                <CreateNewFolderTwoToneIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                <TextField id="standard-basic" label="Folder name" variant="standard"/>
-            </Box>
-            <Box sx={{ '& button': { m: 1 } }}>
-                <div>
-                    <Button size="small">Small</Button>
-                    <Button size="medium">Medium</Button>
-                    <Button size="large">Large</Button>
-                </div>
-                <div>
-                    <Button variant="outlined" size="small">
-                        Small
-                    </Button>
-                    <Button variant="outlined" size="medium">
-                        Medium
-                    </Button>
-                    <Button variant="outlined" size="large">
-                        Large
-                    </Button>
-                </div>
-                <div>
-                    <Button variant="contained" size="small">
-                        Small
-                    </Button>
-                    <Button variant="contained" size="medium">
-                        Medium
-                    </Button>
-                    <Button variant="contained" size="large">
-                        Large
-                    </Button>
-                </div>
-            </Box>
-        </React.Fragment>
+        <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+            <Grid item xs={12} md={6}>
+                <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                    Avatar with text and icon
+                </Typography>
+                <Demo>
+                    <List dense={dense}>
+                        {generate(
+                            <ListItem
+                                secondaryAction={
+                                    <IconButton edge="end" aria-label="delete">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                }
+                            >
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <FolderIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="Single-line item"
+                                    secondary={secondary ? 'Secondary text' : null}
+                                />
+                            </ListItem>,
+                        )}
+                    </List>
+                </Demo>
+            </Grid>
+        </Box>
     );
 };
 
---
+++ pass-react/src/component/Pages/Password/Password.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Password = () => {
+    return (
+        <div>
+            Passwords
+        </div>
+    );
+};
+
+export default Password;
