2f7c3f1730366e718f76861c59a47ba9be712c16
blaec <odeskonst@yandex.ru>
2022-01-22 13:48:40 +0200

    #15 fix menu

--- pass-react/src/component/Toolbar/components/MyDrawer.js
+++ pass-react/src/component/Toolbar/components/MyDrawer.js
@@ -6,15 +6,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import {GetMenuItemsBlock, MainItemsData, SettingsItemsData} from "./MyMenu";
 
 const drawerWidth = 240;
 
@@ -31,25 +27,11 @@ function MyDrawer(props) {
             <Toolbar />
             <Divider />
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
+                {GetMenuItemsBlock(MainItemsData)}
             </List>
             <Divider />
             <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
+                {GetMenuItemsBlock(SettingsItemsData)}
             </List>
         </div>
     );
@@ -112,39 +94,6 @@ function MyDrawer(props) {
                 >
                     {drawer}
                 </Drawer>
-            </Box>
-            <Box
-                component="main"
-                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-            >
-                <Toolbar />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                    sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                    posuere sollicitudin aliquam ultrices sagittis orci a.
-                </Typography>
             </Box>
         </Box>
     );
--- pass-react/src/component/Toolbar/components/MyMenu.js
+++ pass-react/src/component/Toolbar/components/MyMenu.js
@@ -5,17 +5,10 @@ import {reactLinks} from "../../../utils/UrlUtils";
 
 import VpnKeyTwoToneIcon from '@mui/icons-material/VpnKeyTwoTone';
 import SettingsApplicationsTwoToneIcon from '@mui/icons-material/SettingsApplicationsTwoTone';
-import Toolbar from "@mui/material/Toolbar";
-import {Divider, ListItem, ListItemButton, ListItemIcon} from "@mui/material";
-import {List} from "@mui/icons-material";
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import Box from "@mui/material/Box";
 
-const getMenuItemsBlock = (itemsData) => {
+export const GetMenuItemsBlock = (itemsData) => {
 
-    return  itemsData.map((item, index) => {
+    return itemsData.map((item, index) => {
             const {text, link, icon} = item;
 
             return (
@@ -30,64 +23,18 @@ const getMenuItemsBlock = (itemsData) => {
     );
 }
 
-const mainItemsData = [
+export const MainItemsData = [
     {
         text: "Passwords",
         link: reactLinks.passwords,
         icon: <VpnKeyTwoToneIcon/>
     },
 ];
-const settingsItemsData = [
+
+export const SettingsItemsData = [
     {
         text: "Settings",
         link: reactLinks.settings,
         icon: <SettingsApplicationsTwoToneIcon/>
     },
 ];
-
-const MyMenu = () => {
-    return (
-        <React.Fragment>
-            <Toolbar/>
-            <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                <nav aria-label="main mailbox folders">
-                    <List>
-                        <ListItem >
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <InboxIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Inbox" />
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <DraftsIcon />
-                                </ListItemIcon>
-                                <ListItemText primary="Drafts" />
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                </nav>
-                <Divider />
-                <nav aria-label="secondary mailbox folders">
-                    <List>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemText primary="Trash" />
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem disablePadding>
-                            <ListItemButton component="a" href="#simple-list">
-                                <ListItemText primary="Spam" />
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                </nav>
-            </Box>
-        </React.Fragment>
-    )
-};
-
-export default MyMenu;
