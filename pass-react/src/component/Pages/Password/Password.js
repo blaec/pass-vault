7f9811a6b88c9bf9b09ea71150a3fd21532baea3
blaec <odeskonst@yandex.ru>
2022-02-10 22:52:09 +0200

    #21 create password form

--- pass-react/src/component/Pages/Password/Password.js
+++ pass-react/src/component/Pages/Password/Password.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
-import Box from "@mui/material/Box";
+import {Card, CardActions, CardContent, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 import {useSelector} from "react-redux";
 import {isArrayExist} from "../../../utils/Utils";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Password = () => {
     const [age, setAge] = React.useState('');
@@ -21,9 +22,9 @@ const Password = () => {
     const menuItems = folderItems.map(fi => <MenuItem key={fi.id} value={fi.id}>{fi.name}</MenuItem>)
 
     return (
-        <>
-            <Box sx={{ minWidth: 120 }}>
-                <FormControl sx={{ m: 1, width: 300 }}>
+        <Card variant="elevation" sx={{width: 300}}>
+            <CardContent>
+                <FormControl fullWidth>
                     <InputLabel>{label}</InputLabel>
                     <Select
                         value={age}
@@ -32,8 +33,55 @@ const Password = () => {
                         {menuItems}
                     </Select>
                 </FormControl>
-            </Box>
-        </>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Title"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="User"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    variant="standard"
+                />
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="name"
+                    label="Website"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                />
+                <TextField
+                    label="Note"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    variant="standard"
+                />
+            </CardContent>
+            <CardActions>
+                <Button>Cancel</Button>
+                <Button>Save</Button>
+            </CardActions>
+        </Card>
     );
 };
 
