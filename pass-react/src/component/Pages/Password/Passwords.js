72c8ffc05a723239e13634770797df7f5b1f5325
blaec <odeskonst@yandex.ru>
2022-02-18 22:32:34 +0200

    #23 add password details drawer

--- pass-react/src/component/Pages/Password/Passwords.js
+++ pass-react/src/component/Pages/Password/Passwords.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import {DataGrid} from '@mui/x-data-grid';
 import {useSelector} from "react-redux";
+import {Drawer} from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
     { field: 'title', headerName: 'Title', width: 130 },
-    { field: 'user', headerName: 'User', width: 130 },
-    { field: 'password', headerName: 'Password', width: 130 },
-    { field: 'website', headerName: 'Website', width: 130 },
-    { field: 'note', headerName: 'Note', width: 130 },
-    { field: 'creationDate', headerName: 'Creation date', width: 130 },
+    // { field: 'user', headerName: 'User', width: 130 },
+    // { field: 'password', headerName: 'Password', width: 130 },
+    // { field: 'website', headerName: 'Website', width: 130 },
+    // { field: 'note', headerName: 'Note', width: 130 },
+    // { field: 'creationDate', headerName: 'Creation date', width: 130 },
     // {
     //     field: 'age',
     //     headerName: 'Age',
@@ -28,17 +31,59 @@ const columns = [
 ];
 
 const Passwords = () => {
+    const [showDetails, setShowDetails] = React.useState(false);
+
     const {passwords, isPasswordsLoaded} = useSelector(state => state.password.passwords);
+
+    const handleRowClick = (params) => {
+        const {row: {id, title, user, password}} = params;
+        const match = passwords.find(pass => pass.id === id);
+
+        // alert(`${id} - ${title} - ${user} - ${password}`);
+        setShowDetails(true);
+    };
+
+    const handleCloseDetails = () => {
+        setShowDetails(false);
+    };
+
+    let data = null;
+    if (isPasswordsLoaded) {
+        data = (
+            <>
+                <DataGrid
+                    rows={passwords}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    checkboxSelection
+                    disableSelectionOnClick={true}
+                    onRowClick={handleRowClick}
+                />
+                <Drawer
+                    anchor='right'
+                    open={showDetails}
+                    hideBackdrop={true}
+                    // onClose={handleCloseDetails}
+                    // onClose={toggleDrawer(anchor, false)}
+                    // onOpen={toggleDrawer(anchor, true)}
+                >
+                    <Box
+                        sx={{ width: 250 }}
+                        // role="presentation"
+                        // onClick={toggleDrawer(anchor, false)}
+                        // onKeyDown={toggleDrawer(anchor, false)}
+                    >
+                        <Button onClick={handleCloseDetails}>Cancel</Button>
+                    </Box>
+                </Drawer>
+            </>
+        );
+    }
 
     return (
         <div style={{ height: 400, width: '100%' }}>
-            <DataGrid
-                rows={passwords}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-            />
+            {data}
         </div>
     );
 };
