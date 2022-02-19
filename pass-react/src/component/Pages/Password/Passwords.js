0616137145862787cb4133ff598301457c115740
blaec <odeskonst@yandex.ru>
2022-02-19 11:28:19 +0200

    #23 display password details

--- pass-react/src/component/Pages/Password/Passwords.js
+++ pass-react/src/component/Pages/Password/Passwords.js
@@ -6,13 +6,13 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
+    // { field: 'id', headerName: 'ID', width: 70 },
     { field: 'title', headerName: 'Title', width: 130 },
     // { field: 'user', headerName: 'User', width: 130 },
     // { field: 'password', headerName: 'Password', width: 130 },
     // { field: 'website', headerName: 'Website', width: 130 },
     // { field: 'note', headerName: 'Note', width: 130 },
-    // { field: 'creationDate', headerName: 'Creation date', width: 130 },
+    { field: 'creationDate', headerName: 'Creation date', width: 130 },
     // {
     //     field: 'age',
     //     headerName: 'Age',
@@ -32,14 +32,13 @@ const columns = [
 
 const Passwords = () => {
     const [showDetails, setShowDetails] = React.useState(false);
+    const [selectedPassword, setSelectedPassword] = React.useState({});
 
     const {passwords, isPasswordsLoaded} = useSelector(state => state.password.passwords);
 
     const handleRowClick = (params) => {
         const {row: {id, title, user, password}} = params;
-        const match = passwords.find(pass => pass.id === id);
-
-        // alert(`${id} - ${title} - ${user} - ${password}`);
+        setSelectedPassword(passwords.find(pass => pass.id === id));
         setShowDetails(true);
     };
 
@@ -69,12 +68,27 @@ const Passwords = () => {
                     // onOpen={toggleDrawer(anchor, true)}
                 >
                     <Box
-                        sx={{ width: 250 }}
+                        sx={{ width: 1250 }}
                         // role="presentation"
                         // onClick={toggleDrawer(anchor, false)}
                         // onKeyDown={toggleDrawer(anchor, false)}
                     >
                         <Button onClick={handleCloseDetails}>Cancel</Button>
+                        <Box>
+                            {selectedPassword.title}
+                        </Box>
+                        <Box>
+                            {selectedPassword.user}
+                        </Box>
+                        <Box>
+                            {selectedPassword.password}
+                        </Box>
+                        <Box>
+                            {selectedPassword.note}
+                        </Box>
+                        <Box>
+                            {selectedPassword.creationDate}
+                        </Box>
                     </Box>
                 </Drawer>
             </>
