5ef9864cd930fb47d49ca8d8ae198ef444ab8d75
blaec <odeskonst@yandex.ru>
2022-04-02 11:20:34 +0300

    #27 password input element

--- pass-react/src/component/Pages/NewPassword/NewPassword.js
+++ pass-react/src/component/Pages/NewPassword/NewPassword.js
@@ -7,13 +7,27 @@ import {savePassword} from "../../../store/state/password/password-actions";
 import {reactLinks} from "../../../utils/UrlUtils";
 import PasswordElement from "./components/PasswordElement";
 
-import {Card, CardActions, CardContent, FormControl, Grid, InputLabel, MenuItem, Select} from "@mui/material";
+import {
+    Card,
+    CardActions,
+    CardContent,
+    FilledInput,
+    FormControl,
+    Grid,
+    InputAdornment,
+    InputLabel,
+    MenuItem,
+    Select
+} from "@mui/material";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 
 
 const NewPassword = () => {
     const [folder, setFolder] = React.useState('');
+    const [showPassword, setShowPassword] = React.useState(false);
     const {folders, isFoldersLoaded} = useSelector(state => state.folder.folders);
     const navigate = useNavigate();
     const titleRef = React.useRef();
@@ -86,12 +100,26 @@ const NewPassword = () => {
                         label={"User"}
                         type={"text"}
                     />
-                    <PasswordElement
-                        style={_element}
-                        elemRef={passwordRef}
-                        label={"Password"}
-                        type={"password"}
-                    />
+                    <FormControl fullWidth sx={_element} variant="filled">
+                        <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+                        <FilledInput
+                            inputRef={passwordRef}
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={() => setShowPassword(!showPassword)}
+                                        onMouseDown={() => setShowPassword(!showPassword)}
+                                        edge="end"
+                                    >
+                                        {showPassword ? <VisibilityOff/> : <Visibility/>}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                    </FormControl>
                     <Grid
                         container
                         direction="row"
