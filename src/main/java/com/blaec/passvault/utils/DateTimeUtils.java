e7f98fa46f56224ce45b576c52fc43dcfa4cb319
blaec <odeskonst@yandex.ru>
2022-09-12 23:31:16 +0300

    #40 update creation date in db

---
+++ src/main/java/com/blaec/passvault/utils/DateTimeUtils.java
@@ -0,0 +1,8 @@
+package com.blaec.passvault.utils;
+
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeUtils {
+    public final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+}
