263b53d8a474b3a3689c884d8873f01e92b90169
blaec <odeskonst@yandex.ru>
2022-09-24 13:23:42 +0300

    #45 remove old code

--- pass-react/src/store/state/index.js
+++ pass-react/src/store/state/index.js
@@ -2,17 +2,14 @@ import {configureStore} from "@reduxjs/toolkit";
 
 import folderReducer from './folder/folder-slice'
 import itemReducer from './item/item-slice'
-import passwordReducer from './password/password-slice'
 import passgenReducer from './passgen/passgen-slice'
 import secretNoteReducer from './secretNote/secret-note-slice'
-
 
 
 const store = configureStore({
     reducer: {
         folder: folderReducer,
         item: itemReducer,
-        password: passwordReducer,
         passgen: passgenReducer,
         secretNote: secretNoteReducer,
     },
--- src/test/java/com/blaec/passvault/controller/PasswordControllerTest.java
+++ src/test/java/com/blaec/passvault/controller/PasswordControllerTest.java
@@ -6,11 +6,8 @@ import com.blaec.passvault.service.ItemService;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 public class PasswordControllerTest extends AbstractControllerTest {
     @Autowired private FolderService folderService;
@@ -26,12 +23,12 @@ public class PasswordControllerTest extends AbstractControllerTest {
     @Test
     @Order(20)
     void getAll() throws Exception {
-        final String url = String.format("%s/get-all", PasswordController.URL);
+//        final String url = String.format("%s/get-all", PasswordController.URL);
 
 //        TestMatcher matcher = getTestMatcher();
-        ResultActions resultActions = perform(MockMvcRequestBuilders.get(url));
-        validate(resultActions)
-                .andExpect(jsonPath("$.*").isNotEmpty())
+//        ResultActions resultActions = perform(MockMvcRequestBuilders.get(url));
+//        validate(resultActions)
+//                .andExpect(jsonPath("$.*").isNotEmpty())
 //                .andExpect(matcher.containsAll(MOVIES))
         ;
     }
