7777315eee809b444de105ba6a0b047e397a5f25
blaec <odeskonst@yandex.ru>
2022-10-23 22:16:25 +0300

    #46 add search UI

--- pass-react/src/component/Toolbar/components/MyAppBar.js
+++ pass-react/src/component/Toolbar/components/MyAppBar.js
@@ -2,42 +2,47 @@ import React from 'react';
 
 import {drawer} from "../../../utils/Constants";
 
+import MySearch from "../../../UI/MySearch";
+
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import Typography from "@mui/material/Typography";
+
+const _iconButton = {
+    mr: 2,
+    display: {sm: 'none'}
+};
+const _appBar = {
+    width: {sm: `calc(100% - ${drawer.width}px)`},
+    ml: {sm: `${drawer.width}px`},
+    zIndex: (theme) => theme.zIndex.drawer + 1,
+};
+const _search = {
+    display: 'flex',
+    justifyContent: 'flex-end'
+};
+
 
 const MyAppBar = (props) => {
     const {onToggle} = props;
 
-    const iconButton = {
-        mr: 2,
-        display: {sm: 'none'}
-    };
-    const appBar = {
-        width: {sm: `calc(100% - ${drawer.width}px)`},
-        ml: {sm: `${drawer.width}px`},
-        zIndex: (theme) => theme.zIndex.drawer + 1,
-    };
 
     return (
         <AppBar
             position="fixed"
-            sx={appBar}
+            sx={_appBar}
         >
-            <Toolbar>
+            <Toolbar sx={_search}>
                 <IconButton
-                    sx={iconButton}
+                    sx={_iconButton}
                     color="inherit"
                     edge="start"
                     onClick={onToggle}
                 >
                     <MenuIcon/>
                 </IconButton>
-                <Typography variant="h6" noWrap component="div">
-                    Responsive drawer
-                </Typography>
+                <MySearch/>
             </Toolbar>
         </AppBar>
     );
