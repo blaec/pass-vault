fba517ad106ac291be777ef192fc7bb98f49218c
blaec <odeskonst@yandex.ru>
2022-10-27 21:47:01 +0300

    #65 replace classes with record

--- src/main/java/com/blaec/passvault/model/to/passgen/PasswordConfigTo.java
+++ src/main/java/com/blaec/passvault/model/to/passgen/PasswordConfigTo.java
@@ -1,16 +1,10 @@
 package com.blaec.passvault.model.to.passgen;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Getter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class PasswordConfigTo {
-    private final int length;
-    private final boolean isUseUpperCase;
-    private final boolean isUseSpecialChars;
-    private final boolean isUseDigits;
+public record PasswordConfigTo
+        (
+                int length,
+                boolean isUseUpperCase,
+                boolean isUseSpecialChars,
+                boolean isUseDigits
+        ) {
 }
