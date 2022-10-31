142c10c86a6362e1a8264da398016eb374fb82c0
blaec <odeskonst@yandex.ru>
2022-10-31 22:55:30 +0200

    #53 refactoring

--- pass-react/src/component/Items/component/ItemDataRow.js
+++ pass-react/src/component/Items/component/ItemDataRow.js
@@ -11,36 +11,42 @@ import {Chip} from "@mui/material";
 import WarningTwoToneIcon from '@mui/icons-material/WarningTwoTone';
 import ErrorTwoToneIcon from '@mui/icons-material/ErrorTwoTone';
 
-
-const ItemDataRow = (props) => {
-    const {id, description, value, isHideIcons, isHidden, isPassword, icon, passwordAgeLeft} = props;
-
-    const _description = {width: '25%'};
-    const _value = {width: '65%'};
-    const _icons = {width: '10%'};
-
-    let displayValue = isHidden
-        ? convertToPassword(value)
-        : isPassword ? <ColorizedPass pass={value}/> : value;
-    if (passwordAgeLeft < passwordAgeLevel.error) {
-        displayValue = (
+const _description = {width: '25%'};
+const _value = {width: '65%'};
+const _icons = {width: '10%'};
+const updateByAge = (value, age) => {
+    let output = value;
+    if (age < passwordAgeLevel.error) {
+        output = (
             <Chip
                 icon={<ErrorTwoToneIcon/>}
                 color="error"
                 variant="outlined"
-                label={displayValue}
+                label={value}
             />
         );
-    } else if (passwordAgeLeft < passwordAgeLevel.warning) {
-        displayValue = (
+    } else if (age < passwordAgeLevel.warning) {
+        output = (
             <Chip
                 icon={<WarningTwoToneIcon/>}
                 color="warning"
                 variant="outlined"
-                label={displayValue}
+                label={value}
             />
         );
     }
+
+    return output;
+}
+
+
+const ItemDataRow = (props) => {
+    const {id, description, value, passwordAgeLeft, icon, isHideIcons, isHidden, isPassword} = props;
+
+    let displayValue = isHidden
+        ? convertToPassword(value)
+        : isPassword ? <ColorizedPass pass={value}/> : value;
+    displayValue = updateByAge(displayValue, passwordAgeLeft);
     const icons = isHideIcons
         ? null
         : (
