57232360d8552c248bae5937c499e025874709b2
blaec <odeskonst@yandex.ru>
2022-11-02 23:32:39 +0200

    #64 add warning

--- pass-react/src/component/Items/component/itemControls/ItemControls.js
+++ pass-react/src/component/Items/component/itemControls/ItemControls.js
@@ -11,11 +11,32 @@ import CancelTwoToneIcon from "@mui/icons-material/CancelTwoTone";
 import Box from "@mui/material/Box";
 import DeletedItemControls from "./DeletedItemControls";
 import ActiveItemControls from "./ActiveItemControls";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
 
 
 const ItemControls = (props) => {
     const {id, type, onEdit, onClose} = props;
     const {pathname} = useLocation();
+
+    const [dialog, setDialog] = React.useState({
+        id: null,
+        action: null,
+        folder: '',
+        title: '',
+        ok: '',
+        cancel: '',
+        message: '',
+        isOpen: false
+    });
+
+    const handleCloseDialog = () => {
+        setDialog({...dialog, isOpen: false})
+    };
 
     const dispatch = useDispatch();
 
@@ -25,6 +46,10 @@ const ItemControls = (props) => {
 
     const handleMoveToFolder = () => {
         alert("move to folder");
+    };
+
+    const handlePrepareMoveToDelete = () => {
+        setDialog({...dialog, isOpen: true});
     };
 
     const handleMoveToTrash = () => {
@@ -46,7 +71,7 @@ const ItemControls = (props) => {
         ? (
             <DeletedItemControls
                 onRestore={handleRestoreFromTrash}
-                onDelete={handleDelete}
+                onDelete={handlePrepareMoveToDelete}
             />
         )
         : (
@@ -74,6 +99,18 @@ const ItemControls = (props) => {
             <Box>
                 {actionIcons}
             </Box>
+            <Dialog open={dialog.isOpen} onClose={handleCloseDialog}>
+                <DialogTitle>Delete Item</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        This item will be lost and you will no longer be able to restore it.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseDialog}>Cancel</Button>
+                    <Button onClick={handleDelete}>Delete</Button>
+                </DialogActions>
+            </Dialog>
         </Grid>
     );
 };
