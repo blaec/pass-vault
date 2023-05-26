b492cd09cc9a7fb1bc3b7b7a5345d3527863038b
blaec <odeskonst@yandex.ru>
2023-05-26 23:18:36 +0300

    #54 Copy to clipboard not working for android & external ip

--- pass-react/src/axios-pass.js
+++ pass-react/src/axios-pass.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://10.100.102.4:8082/api/v1/'
+    baseURL: 'https://10.100.102.4:8082/api/v1/'
 });
 
 export default instance;
