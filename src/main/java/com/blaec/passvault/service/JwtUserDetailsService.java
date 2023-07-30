fdf6788d547313eb7be4f7fbdb7697d4efd8f1f2
blaec <odeskonst@yandex.ru>
2023-07-30 23:40:52 +0300

    #78 refactoring

--- src/main/java/com/blaec/passvault/service/JwtUserDetailsService.java
+++ src/main/java/com/blaec/passvault/service/JwtUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.blaec.passvault.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,11 +11,13 @@ import java.util.ArrayList;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
+	@Value("${jwt.secret}")
+	private String secret;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		if ("javainuse".equals(username)) {
-			return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+		if (secret.equals(username)) {
+			return new User(secret, "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
 					new ArrayList<>());
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
