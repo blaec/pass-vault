51666d6baee060f0412b0c9ed98c7cf06636b2e9
blaec <odeskonst@yandex.ru>
2023-07-31 21:59:26 +0300

    #78 refactoring

--- src/main/java/com/blaec/passvault/model/JwtResponse.java
+++ src/main/java/com/blaec/passvault/model/JwtResponse.java
@@ -1,19 +1,12 @@
 package com.blaec.passvault.model;
 
-import java.io.Serial;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.io.Serializable;
 
+@AllArgsConstructor
+@Getter
 public class JwtResponse implements Serializable {
-
-	@Serial
-	private static final long serialVersionUID = -8091879091924046844L;
-	private final String jwttoken;
-
-	public JwtResponse(String jwttoken) {
-		this.jwttoken = jwttoken;
-	}
-
-	public String getToken() {
-		return this.jwttoken;
-	}
+	private final String token;
 }
